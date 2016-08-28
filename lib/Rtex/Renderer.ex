7121bf5156708f7679d7da38e8e350896dc0eb7b
Geoff Stockham <geoff@stormid.com>
2016-08-28 21:07:33 +0100

    Gradient background

--- lib/Rtex/Renderer.ex
+++ lib/Rtex/Renderer.ex
@@ -28,11 +28,37 @@ defmodule Rtex.Renderer do
         |> Rtex.Math.Vec3.add(Rtex.Math.Vec3.scale(u, scene.camera.horizontal))
         |> Rtex.Math.Vec3.add(Rtex.Math.Vec3.scale(v, scene.camera.vertical))
     ray = Rtex.Ray.create(scene.camera.position, direction)
-    colour = get_colour(scene, ray)
-    {col, row, colour}
+    case hit_object(ray, scene) do
+      {true, model} -> {col, row, get_colour(ray, model, scene)}
+      {false, _} -> {col, row, get_background_colour(ray, scene)}
+    end
+   end
+
+  def hit_object(ray, scene) do
+    hit_any_object(scene.models, ray, 0.1, 1_000_000, nil)
   end
 
-  def get_colour(scene, ray) do
+  def hit_any_object([head|tail], ray, t_min, t_max, current_nearest) do
+    case Rtex.GeometricObject.hit(head, ray, t_min) do
+      {true, t} -> 
+        if t < t_max do
+          hit_any_object(tail, ray, t_min, t, head)
+        else 
+          hit_any_object(tail, ray, t_min, t_max, current_nearest)
+        end
+      {false, _} -> 
+        hit_any_object(tail, ray, t_min, t_max, current_nearest)
+    end
+  end
+
+  def hit_any_object([], ray, t_min, t_max, current_nearest) do
+    case current_nearest do
+      nil -> {false, nil}
+      _ -> {true, current_nearest}
+    end
+  end
+  
+  def get_background_colour(ray, scene) do
     {x, y, z} = Rtex.Math.Vec3.unit(ray.direction)
     t = 0.5 * (y + 1)
     {1.0, 1.0, 1.0}
